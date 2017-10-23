d7ebd2532fe85757392ad1a8dcdb42181217dedf
silvercrow <sc@silvercrow.org>
2017-10-23 00:17:31 -0600

    Added share link using facebook sdk to map share button

--- src/routes/Map/Footer.js
+++ src/routes/Map/Footer.js
@@ -1,28 +1,52 @@
 import React from 'react'
 
-export const Footer = () => (
-	<div>
-        <div className="container">
-            <a href="https://www.facebook.com/guatevoz/" target="_blank">
-                <button className="gtv-shareBtn"><span>Compartir en Facebook</span></button>
-            </a>
-        </div>
+class Footer extends React.Component {
 
-        <footer className="gtv-footer">
-            <div className="container">
-                <div className="gtv-fbShare">
-                    <a href="https://www.facebook.com/GuateVozUSA/" target="_blank">
-                        <span>www.facebook.com/GuateVoz</span>
-                    </a>
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+        //Binding to view
+        this.shareToFacebook = this
+            .shareToFacebook
+            .bind(this);
+
+    }
+
+    shareToFacebook() {
+        FB
+            .ui({
+                method: 'share',
+                display: 'popup',
+                href: 'https://www.facebook.com/GuateVozUSA/'
+            }, function (response) {});
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <button className="gtv-shareBtn" onClick={this.shareToFacebook}>
+                        <span>Compartir en Facebook</span>
+                    </button>
                 </div>
-                <p className="gtv-footerText">2017 GuateVoz.com Todos los derechos reservados.</p>
-            </div>
-        </footer>
 
-    </div>
-)
+                <footer className="gtv-footer">
+                    <div className="container">
+                        <div className="gtv-fbShare">
+                            <a href="https://www.facebook.com/GuateVozUSA/" target="_blank">
+                                <span>www.facebook.com/GuateVoz</span>
+                            </a>
+                        </div>
+                        <p className="gtv-footerText">2017 GuateVoz.com Todos los derechos reservados.</p>
+                    </div>
+                </footer>
+
+            </div>
+        );
+
+    }
+
+}
 
 export default Footer
-
-
-
